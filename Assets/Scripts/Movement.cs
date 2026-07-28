f240ba818553466ec12e7e414b32095182e90fba
Basil Barnaby <basilbarnaby.ca@gmail.com>
2026-07-28 00:15:52 -0400

    feat(player): added thrust, rotation, and cinemachine

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,17 +4,58 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
 	[SerializeField] InputAction thrust;
+	[SerializeField] InputAction rotation;
+	[SerializeField] float thrustStrength = 10f;
+	[SerializeField] float rotationStrength = 100f;
+	Rigidbody rb;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+
+	}
 
 	private void OnEnable()
 	{
 		thrust.Enable();
+		rotation.Enable();
 	}
 
-	private void Update()
+	private void FixedUpdate()
+	{
+		IgniteEngine();
+		ActivateRotation();
+	}
+
+	private void IgniteEngine()
 	{
 		if (thrust.IsPressed())
 		{
-			Debug.Log("Engines full power!");
+			rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
 		}
+	}
+
+	private void ActivateRotation()
+	{
+		float rotationInput = rotation.ReadValue<float>();
+
+		if (rotationInput < 0)
+		{
+			ApplyRotation(rotationStrength);
+			Debug.Log("Rotation Left");
+		}
+
+		else if (rotationInput > 0)
+		{
+			ApplyRotation(-rotationStrength);
+			Debug.Log("Rotation Right");
+
+		}
+
+	}
+
+	private void ApplyRotation(float rotationThisFrame)
+	{
+		transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
 	}
 }
